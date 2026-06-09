45356a12985a749d316cf04e5d0f781470a7c4b8
joel.cranmer <joel.cranmer.ctr@afacademy.af.edu>
2026-06-09 10:47:05 -0600

    clean up some test cases
    Signed-off-by: joel.cranmer <joel.cranmer.ctr@afacademy.af.edu>

--- tests/core/test_artifact.py
+++ tests/core/test_artifact.py
@@ -15,7 +15,8 @@ class TestBalanceCalibration(unittest.TestCase):
         cls.fixture_dir = os.path.join(cls.base_dir, 'fixtures')
         cls.metadata = CalibrationMetadata(
             author="System",
-            balance_info={"test_balance": True}
+            balance_info={"test_balance": True},
+            distances=(1, 2, 3, 4)
         )
 
     def test_load_valid_file(self):
@@ -25,9 +26,12 @@ class TestBalanceCalibration(unittest.TestCase):
         self.assertEqual(Cal.coefficient_matrix[1, 3], 0.024591532002882124)
 
     def test_load_invalid_file(self):
+        """If the file doesn't have the *balance_calibration* property it should fail to load"""
         file_path = os.path.join(self.fixture_dir, "Test_bad_cal.json")
         Cal = BalanceCalibration.load(file_path)
+
         self.assertEqual(Cal.__class__.__name__, "NoneType")
+        self.assertEqual(Cal, None)
 
     def test_save_cal_file(self):
         # dummy 6x6 matrix
@@ -41,7 +45,8 @@ class TestBalanceCalibration(unittest.TestCase):
         ])
         bias = np.array([0, 0, 0, 0, 0, 0])
         x = BalanceCalibration.create_5f1m_transformation_matrix(1.5, 1.5, 1.25, 1.25)
-        cal = BalanceCalibration(C, "LinearModel", ["N1", "N2", "Y1", "Y2", "AF", "RM"], bias, x, self.metadata)
+        channels = ["N1", "N2", "Y1", "Y2", "AF", "RM"]
+        cal = BalanceCalibration(C, "LinearModel", channels, bias, x, self.metadata)
 
         with tempfile.TemporaryDirectory() as temp_dir:
             demo_file = os.path.join(temp_dir, "tmp_file.json")
@@ -51,6 +56,12 @@ class TestBalanceCalibration(unittest.TestCase):
                 lines = f.readlines()
 
             self.assertTrue(len(lines) >= 126)
+            # Load the file and make sure everything is the same
+            new_cal = BalanceCalibration.load(demo_file)
+
+            np.testing.assert_array_almost_equal(C, new_cal.coefficient_matrix)
+            np.testing.assert_array_almost_equal(x, new_cal.transformation_matrix)
+            self.assertEqual(channels, new_cal.component_names)
 
 
 class Test5F1MTransformationMatrix(unittest.TestCase):
