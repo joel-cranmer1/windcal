d64c36ac9feac4bf18c5005c8f4ab9295685f19a
joel.cranmer <joel.cranmer@afacademy.af.edu>
2026-06-24 15:54:02 -0600

    Create test for LinearAbsoluteModel
    Signed-off-by: joel.cranmer <joel.cranmer@afacademy.af.edu>

---
+++ tests/calibration/test_lin_abs_model.py
@@ -0,0 +1,6 @@
+from windcal.calibration.models import LinearAbsoluteModel
+from .test_model_base import CalibrationModelContractTest
+
+
+class TestLinearAbsoluteModel(CalibrationModelContractTest):
+    MODEL_CLASS = LinearAbsoluteModel
